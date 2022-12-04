1b4ad5903996a519e4e416ca8a8f31c4824ee864
henatic <hdmorgan4@gmail.com>
2022-12-04 00:33:10 -0800

    Completed Day 4.

--- Day_4/Camp_Cleanup.py
+++ Day_4/Camp_Cleanup.py
@@ -3,6 +3,7 @@ f = open("input.txt", "r")
 
 # Initialize the number of completely contained ranges.
 numContain = 0
+numOverlap = 0
 
 # Iterate to find how many ranges have been contained.
 for x in f:
@@ -34,6 +35,13 @@ for x in f:
     # If so, we increment the number of contained set of ranges.
     if (int(range1[0:dash1]) >= int(range2[0:dash2]) and int(range1[dash1 + 1:]) <= int(range2[dash2 + 1:])) or (int(range1[0:dash1]) <= int(range2[0:dash2]) and int(range1[dash1 + 1:]) >= int(range2[dash2 + 1:])):
         numContain += 1
+        numOverlap += 1
+
+    # If not, we determine if the ranges overlap at all.
+    # If so, we increment the number of overlapping set of ranges.
+    elif (int(range1[0:dash1]) <= int(range2[dash2 + 1:]) and int(range1[0:dash1]) >= int(range2[0:dash2])) or (int(range1[dash1 + 1:]) <= int(range2[dash2 + 1:]) and int(range1[dash1 + 1:]) >= int(range2[0:dash2])):
+        numOverlap += 1
 
 # Print final results.
-print(numContain)
+print("Number of contained assignments: " + str(numContain))
+print("Number of overlapping assignments: " + str(numOverlap))
