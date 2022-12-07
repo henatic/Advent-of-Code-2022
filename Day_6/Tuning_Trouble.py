34ddf08183568df3375b6bd68ecc3f8bc212e6a7
henatic <hdmorgan4@gmail.com>
2022-12-06 23:35:10 -0800

    Completed comments.

--- Day_6/Tuning_Trouble.py
+++ Day_6/Tuning_Trouble.py
@@ -51,22 +51,42 @@ while len(msgString) < 14:
 
 # Iterate through our packet until we get 14 unique characters.
 while i < len(datastream):
+
+    # Left index of packet string.
     iter = 0
+
+    # Determines if all our characters are unique.
     noUnique = False
+
+    # Iterate through selected characters to determine of all characters are unique.
     while iter < len(msgString) - 1:
+
+        # Right index to compare to left index.
         char = iter + 1
+
+        # Changes indexes and compares them all.
         while char < len(msgString) and noUnique == False:
+
+            # Set variable to True if string has repeating characters.
             if msgString[iter] == msgString[char]:
                 noUnique = True
             char += 1
         iter += 1
+
+    # If variable still remains false after comparisons, we've found our start of message and we break from the loop.
     if noUnique == False:
         break
+    
+    # Otherwise, we continue looking.
     msgString = msgString[1:] + datastream[i]
     i += 1
 
+# If our index variable is the string length, our code failed to find the start of the packet.
 if i >= len(datastream):
     print("No start of message.")
+
+    # End our program.
     exit(1)
 else:
+    # Otherwise, we print our results for Part 2.
     print("Start of message is at " + str(i) + ".\n" + str(msgString))
